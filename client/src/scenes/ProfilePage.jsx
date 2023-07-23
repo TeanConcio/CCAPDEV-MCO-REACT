f4cb064f9d49646bb90b1b219a98ee3546f53899
Tean Concio <tean_concio@dlsu.edu.ph>
2023-07-23 21:43:55 +0800

    Commit before aldrich leaves

--- client/src/scenes/ProfilePage.jsx
+++ client/src/scenes/ProfilePage.jsx
@@ -28,9 +28,6 @@ const ProfilePage = () => {
     const { userId } = useParams();
     const token = useSelector((state) => state.token);
 
-    // Check screen size
-    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-
 
 
 
@@ -67,11 +64,31 @@ const ProfilePage = () => {
 
     return (
 
-        <Box>
+        <>
 
             <Navbar />
 
-            <Box
+            <div className="page">
+
+                <div className="column">
+
+                    <UserWidget userId={userId} picturePath={user.picturePath} />
+
+                    <FriendListWidget userId={userId} />
+
+                </div>
+
+                <div className="column">
+
+                    <MyPostWidget picturePath={user.picturePath} />
+
+                    <PostsWidget userId={userId} isProfile />
+
+                </div>
+
+            </div>
+
+            {/* <Box
                 width="100%"
                 padding="2rem 6%"
                 display={isNonMobileScreens ? "flex" : "block"}
@@ -79,21 +96,21 @@ const ProfilePage = () => {
                 justifyContent="center"
             >
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-                    <UserWidget userId={userId} picturePath={user.picturePath} />
+                    
                     <Box m="2rem 0" />
-                        <FriendListWidget userId={userId} />
+                        
                     </Box>
                     <Box
                         flexBasis={isNonMobileScreens ? "42%" : undefined}
                         mt={isNonMobileScreens ? undefined : "2rem"}
                     >
-                        <MyPostWidget picturePath={user.picturePath} />
+                        
                     <Box m="2rem 0" />
-                    <PostsWidget userId={userId} isProfile />
+                    
                 </Box>
-            </Box>
+            </Box> */}
 
-        </Box>
+        </>
 
     );
 }
