282d501962e7fd1903341a60274eed0702f192de
Aldrich Go <serocat1211@gmail.com>
2023-08-02 17:23:38 +0800

    user profile changes

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -16,6 +16,8 @@ import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
+// CSS import
+import "../../styles/scenes/widgets/UserProfile.css";
 
 
 
@@ -76,112 +78,47 @@ const UserWidget = ({ userId, picturePath }) => {
         friends,
     } = user;
 
-
-
-
-
     /* RENDER */
 
     return (
+        
+            <div className="userprofilecontainer">
+                <div className="userprofileprofile-card">
+                    <div className="userprofilecover-pic"> Profile
+                        <div className="userprofilebasic-actions">
+                        </div>
+                    </div>
+                    <br />
+                    <UserImage
+                        src={picturePath}
+                        alt="Profile Picture"
+                        className="userprofileprofile-pic"
+                    />
+                    <h1>{username}</h1>
+                    <p>{description}</p>
+                    <div className="userprofilefollow-btn">
+                        Follow
+                    </div>
 
-        <WidgetWrapper>
-
-            {/* FIRST ROW */}
-            <FlexBetween
-                gap="0.5rem"
-                pb="1.1rem"
-                onClick={() => navigate(`/profile/${userId}`)}
-            >
-                <FlexBetween gap="1rem">
-                    <Box>
-                        <Typography
-                        variant="h4"
-                        color={dark}
-                        fontWeight="500"
-                        sx={{
-                            "&:hover": {
-                            color: palette.primary.light,
-                            cursor: "pointer",
-                            },
-                        }}
-                        >
-                            {username}
-                        </Typography>
-                        <Typography color={medium}>
-                            {friends.length} friends
-                        </Typography>
-                    </Box>
-                </FlexBetween>
-                <ManageAccountsOutlined />
-            </FlexBetween>
-
-            <Divider />
-
-            {/* SECOND ROW */}
-            <Box p="1rem 0">
-                <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
-                    <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-                    <Typography color={medium}>{description}</Typography>
-                </Box>
-            </Box>
-
-            <Divider />
-
-            {/* THIRD ROW */}
-            <Box p="1rem 0">
-                <FlexBetween mb="0.5rem">
-                    <Typography color={medium}>Number of Posts</Typography>
-                    <Typography color={main} fontWeight="500">
-                        {postsNum}
-                    </Typography>
-                </FlexBetween>
-                <FlexBetween>
-                    <Typography color={medium}>Total Upvote Count</Typography>
-                    <Typography color={main} fontWeight="500">
-                        {upvotes}
-                    </Typography>
-                </FlexBetween>
-            </Box>
-
-            <Divider />
-
-            {/* FOURTH ROW */}
-            <Box p="1rem 0">
-                <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
-                    Social Profiles
-                </Typography>
-
-                <FlexBetween gap="1rem" mb="0.5rem">
-                    <FlexBetween gap="1rem">
-                        <img src="../assets/twitter.png" alt="twitter" />
-                        <Box>
-                            <Typography color={main} fontWeight="500">
-                                Twitter
-                            </Typography>
-                            <Typography color={medium}>Social Network</Typography>
-                        </Box>
-                    </FlexBetween>
-                    <EditOutlined sx={{ color: main }} />
-                </FlexBetween>
-
-                <FlexBetween gap="1rem">
-                    <FlexBetween gap="1rem">
-                        <img src="../assets/linkedin.png" alt="linkedin" />
-                        <Box>
-                            <Typography color={main} fontWeight="500">
-                                Linkedin
-                            </Typography>
-                            <Typography color={medium}>Network Platform</Typography>
-                        </Box>
-                    </FlexBetween>
-                    <EditOutlined sx={{ color: main }} />
-                </FlexBetween>
-            </Box>
-
-        </WidgetWrapper>
+                    <div className="userprofilerow">
+                        <div>
+                            <p>Upvotes</p>
+                            <h2>{upvotes}</h2>
+                        </div>
+                        <div>
+                            <p>Date Joined</p>
+                            <h2>October 25, 2019</h2>
+                        </div>
+                        <div>
+                            <p>Posts</p>
+                            <h2>{postsNum}</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
     );
 };
-
 
 
 
